c3907710325f6cc8d90a0072411e9b17960a9062
Kevin Carrington <Kevington99@gmail.com>
2024-09-11 19:36:47 -0700

    add player slide. test git control

--- Player.gd
+++ Player.gd
@@ -1,26 +1,21 @@
 extends CharacterBody2D
 
-# Variables for movement
-var speed = 200
+var max_speed = 200
+var acceleration = 1000
+var friction = 800
 
-func _process(_delta):
-	# Initialize the velocity with zero to reset it each frame
-	var direction = Vector2.ZERO
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
 
-	# Move based on input
-	if Input.is_action_pressed("ui_right"):
-		direction.x += 1
-	if Input.is_action_pressed("ui_left"):
-		direction.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		direction.y += 1
-	if Input.is_action_pressed("ui_up"):
-		direction.y -= 1
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * max_speed, acceleration * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
 
-	# Normalize the direction and apply speed if direction is non-zero
-	if direction != Vector2.ZERO:
-		direction = direction.normalized() * speed
-
-	# Set the velocity and move the character
-	velocity = direction
 	move_and_slide()
+
+# Remove or comment out the following line:
+# /test
