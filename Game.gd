666706ba861c9a81299247ffd6d34c3d9558f549
Kevin Carrington <Kevington99@gmail.com>
2024-09-13 20:14:58 -0700

    insert practice sprite. Practice with aesprite and AnimatedSprite2D

--- Game.gd
+++ Game.gd
@@ -1,29 +1,9 @@
 extends Node2D
 
-var trail = []
-const TRAIL_LENGTH = 50
-
 func _ready():
-	center_camera()
-
-func center_camera():
-	var viewport_size = get_viewport().size
-	$Player.position = viewport_size / 2
-	$Player/Camera2D.make_current()
+	pass
 
 func _process(_delta):
-	update_trail()
-	queue_redraw()
-
 	if Input.is_action_just_pressed("ui_accept"):
 		print("Player position: ", $Player.global_position)
 		print("Player velocity: ", $Player.velocity)
-
-func update_trail():
-	trail.push_front($Player.global_position)
-	if trail.size() > TRAIL_LENGTH:
-		trail.pop_back()
-
-func _draw():
-	for i in range(1, trail.size()):
-		draw_line(trail[i-1] - $Player.global_position, trail[i] - $Player.global_position, Color(1, 1, 0), 2)
--- Player.gd
+++ Player.gd
@@ -1,58 +1,24 @@
 extends CharacterBody2D
 
-var speed = 100  # Adjust this value to match Undertale's movement speed
-var trail_points = []
-var max_trail_points = 20
-var trail_length = 50  # Length of the tail in pixels
-var trail_segments = 10  # Number of segments in the tail
+@export var speed = 100
 
-func _ready():
-	# Create a Line2D node for the trail
-	var trail = Line2D.new()
-	trail.name = "Trail"
-	trail.default_color = Color(0.5, 0, 0.5, 0.5)  # Purple with some transparency
-	trail.width = 5
-	trail.joint_mode = Line2D.LINE_JOINT_ROUND
-	trail.begin_cap_mode = Line2D.LINE_CAP_ROUND
-	trail.end_cap_mode = Line2D.LINE_CAP_ROUND
-	add_child(trail)
-	
-	# Initialize trail points
-	for i in range(trail_segments):
-		trail_points.append(position)
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
-	var input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	
-	velocity = input_vector * speed
+	var input_vector = Vector2.ZERO
+	input_vector.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	input_vector.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		$Sprite2D.modulate = Color(1, 0.5, 0.5)
+		velocity = input_vector * speed
+		# Update animation based on movement direction
+		if input_vector.x < 0:
+			animated_sprite.play("walk_left")
+		else:
+			animated_sprite.play("walk_right")
 	else:
-		$Sprite2D.modulate = Color(1, 1, 1)
+		velocity = Vector2.ZERO
+		animated_sprite.play("idle")
 	
 	move_and_slide()
-	
-	# Update trail
-	update_trail(delta)
-
-func update_trail(delta):
-	# Update the first point to the player's current position
-	trail_points[0] = position
-	
-	# Update subsequent points
-	for i in range(1, trail_points.size()):
-		var target_position = trail_points[i-1]
-		var current_position = trail_points[i]
-		var new_position = current_position.lerp(target_position, 15 * delta)
-		trail_points[i] = new_position
-	
-	# Update the Line2D with new points
-	$Trail.clear_points()
-	for point in trail_points:
-		$Trail.add_point(point - position)
-
-func _input(event):
-	if event.is_action_pressed("ui_accept"):
-		print("Player position: ", global_position)
-		print("Player velocity: ", velocity)
