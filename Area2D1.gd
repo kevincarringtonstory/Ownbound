7e9f3dadea010efdd47a6afa5e4caa8913c08483
Kevin Carrington <Kevington99@gmail.com>
2024-09-16 17:00:06 -0700

    learn how to change scenes
    Got a basic scene change going.

---
+++ Area2D1.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+var entered = false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _on_body_entered(body: CharacterBody2D) -> void:
+	entered = true
+
+func _process(delta: float) -> void:
+	if entered == true:
+		get_tree().change_scene_to_file("res://Main.tscn")
