dd33e0e2186d1e52fa2f29396f779e4cf0d26b37
Kevin Carrington <44887007+kevincarringtonstory@users.noreply.github.com>
2024-09-25 13:15:45 -0700

    clean up

--- Game.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-func _ready():
-	pass
-
-func _process(_delta):
-	if Input.is_action_just_pressed("ui_accept"):
-		print("Player position: ", $Player.global_position)
-		print("Player velocity: ", $Player.velocity)
