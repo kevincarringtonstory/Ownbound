5ff399a916067acc7137c1a6e93cbda2fb2b7b4c
Kevin Carrington <44887007+kevincarringtonstory@users.noreply.github.com>
2024-10-05 15:19:48 -0700

    work on sprites

--- player/player.gd
+++ player/player.gd
@@ -5,44 +5,44 @@ extends CharacterBody2D
 
 
 func _unhandled_input(_event: InputEvent) -> void:
-	
-	if Input.is_action_just_pressed("ui_accept"):
-			var actionables = actionable_finder.get_overlapping_areas()
-			if actionables.size() > 0:
-				actionables[0].action()
-				velocity	 = Vector2.ZERO
-			return
-			
-	handle_movement()
+    
+    if Input.is_action_just_pressed("ui_accept"):
+            var actionables = actionable_finder.get_overlapping_areas()
+            if actionables.size() > 0:
+                actionables[0].action()
+                velocity	 = Vector2.ZERO
+            return
+            
+    handle_movement()
 
 func _physics_process(delta: float) -> void:
-	
-	handle_animation()
-	
+    
+    handle_animation()
+    
 #functions
-	
+    
 func handle_movement():
-	velocity = Vector2.ZERO
-	if Input	.is_action_pressed("ui_right"):
-		velocity.x += 1.0
-	if Input	.is_action_pressed("ui_left"):
-		velocity.x -= 1.0
-	if Input	.is_action_pressed("ui_down"):
-		velocity.y += 1.0
-	if Input	.is_action_pressed("ui_up"):
-		velocity.y -= 1.0
-	velocity = velocity.normalized()	
-	self.velocity = velocity * speed		
-	
+    velocity = Vector2.ZERO
+    if Input	.is_action_pressed("ui_right"):
+        velocity.x += 1.0
+    if Input	.is_action_pressed("ui_left"):
+        velocity.x -= 1.0
+    if Input	.is_action_pressed("ui_down"):
+        velocity.y += 1.0
+    if Input	.is_action_pressed("ui_up"):
+        velocity.y -= 1.0
+    velocity = velocity.normalized()	
+    self.velocity = velocity * speed		
+    
 func handle_animation():
-	
-	if velocity == Vector2.ZERO:
-		$AnimationTree.get("parameters/playback").travel("Idle")
-	else:
-		$AnimationTree.get("parameters/playback").travel("Walk")
-		$AnimationTree.set("parameters/Idle/blend_position", velocity)
-		$AnimationTree.set("parameters/Walk/blend_position", velocity)
-	
-	move_and_slide()	
-	
-	
+    
+    if velocity == Vector2.ZERO:
+        $AnimationTree.get("parameters/playback").travel("Idle")
+    else:
+        $AnimationTree.get("parameters/playback").travel("Walk")
+        $AnimationTree.set("parameters/Idle/blend_position", velocity)
+        $AnimationTree.set("parameters/Walk/blend_position", velocity)
+    
+    move_and_slide()	
+    
+    
